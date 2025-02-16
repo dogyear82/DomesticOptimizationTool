8f990e7e16b3e963f832981a978e86b4ab72a255
thnguyen00 <tannguyen@Allcovered.com>
2025-02-16 13:21:39 +0900

    refactored extensions

--- Dot.Services/Messaging/Extensions/MessagingExtensions.cs
+++ Dot.Services/Messaging/Extensions/MessagingExtensions.cs
@@ -2,6 +2,7 @@
 using Dot.Services.Options;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using RabbitMQ.Client;
 
 namespace Dot.Services.Messaging.Extensions
@@ -11,22 +12,27 @@ namespace Dot.Services.Messaging.Extensions
         public static void AddMessageSender(this IServiceCollection services, IConfiguration config)
         {
             services.Configure<RabbitMQSettings>(config.GetSection("RabbitMQSettings"));
-            services.AddSingleton<IConnectionFactory>(sp =>
-            {
-                return new ConnectionFactory() { HostName = "localhost" };
-            });
+            services.AddSingleton(GetConnectionFactory);
             services.AddSingleton<IMessageSender, MessageSender>();
         }
 
         public static void AddMessageReceiver(this IServiceCollection services, IConfiguration config)
         {
             services.Configure<RabbitMQSettings>(config.GetSection("RabbitMQSettings"));
-            services.AddSingleton<IConnectionFactory>(sp =>
-            {
-                return new ConnectionFactory() { HostName = "localhost" };
-            });
+            services.AddSingleton(GetConnectionFactory);
             services.AddSingleton<IMessageProcessor, MessageProcessor>();
         }
 
+        private static IConnectionFactory GetConnectionFactory(IServiceProvider sp)
+        {
+            var rabbitSettings = sp.GetRequiredService<IOptions<RabbitMQSettings>>().Value;
+            return new ConnectionFactory() 
+            {
+                HostName = rabbitSettings.HostName,
+                UserName = rabbitSettings.UserName,
+                Password = rabbitSettings.Password,
+                VirtualHost = "/"
+            };
+        }
     }
 }
