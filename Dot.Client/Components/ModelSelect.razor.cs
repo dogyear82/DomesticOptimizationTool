8c5ff53833af0a1ca6a1925c9303390d2bb879a2
thnguyen00 <tannguyen@Allcovered.com>
2025-03-06 23:00:41 +0900

    added model select component with subbted data

---
+++ Dot.Client/Components/ModelSelect.razor.cs
@@ -0,0 +1,26 @@
+﻿using Dot.API.Gateway;
+using Microsoft.AspNetCore.Components;
+
+namespace Dot.Client.Components
+{
+    public partial class ModelSelect : ComponentBase
+    {
+        [Inject]
+        private IGateway gateway { get; set; }
+
+        protected string selectedModelName = string.Empty;
+        protected List<string> modelNames = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                modelNames = new List<string> { "test1", "test2", "test3" };//await gateway.Llms.GetNamesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
