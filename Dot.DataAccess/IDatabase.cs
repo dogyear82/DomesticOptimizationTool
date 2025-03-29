50c666661e81396bd4ccfdd9841dc44da992248d
thnguyen00 <tannguyen@Allcovered.com>
2025-03-29 14:14:39 +0900

    removed blank line

--- Dot.DataAccess/IDatabase.cs
+++ Dot.DataAccess/IDatabase.cs
@@ -3,7 +3,6 @@
     public interface IDatabase
     {
         Task<T> CreateAsync<T>(T record);
-
         Task<List<T>> ReadAsync<T>();
         Task<T> ReadAsync<T>(string id);
         Task<List<T>> ReadAsync<T>(string filterName, string filterValue);
