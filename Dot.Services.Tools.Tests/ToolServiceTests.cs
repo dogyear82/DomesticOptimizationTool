86f5b011100f37142b11098a7155b7021e76e124
thnguyen00 <tannguyen@Allcovered.com>
2025-04-02 14:32:48 +0900

    added tools tests for tools prompt

---
+++ Dot.Services.Tools.Tests/ToolServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot.Services.Tools.Tests
+{
+    public class ToolServiceTests
+    {
+        private readonly IToolService _toolService;
+
+        public ToolServiceTests()
+        {
+            var tools = new List<ITool>
+            {
+                new Silly(),
+                new GeneralChat()
+            };
+            _toolService = new ToolService(tools);
+        }
+
+        [Fact]
+        public void ConstructToolPrompt_ReturnsPrompt()
+        {
+            var prompt = _toolService.ConstructToolPrompt();
+            Assert.NotNull(prompt);
+        }
+    }
+}
